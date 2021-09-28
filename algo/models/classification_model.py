f1e488ac5681b54e3604fe4b6e7d7b5403b60c98
HHansi <h.hansi1991@gmail.com>
2021-09-28 19:50:18 +0100

    001: Added algo:models

---
+++ algo/models/classification_model.py
@@ -0,0 +1,133 @@
+# Created by Hansi at 9/23/2021
+import logging
+from pathlib import Path
+
+# from FARM2.farm.data_handler.data_silo import DataSilo
+# from FARM2.farm.data_handler.processor import TextClassificationProcessor
+# from FARM2.farm.infer import Inferencer
+# from FARM2.farm.modeling.adaptive_model import AdaptiveModel
+# from FARM2.farm.modeling.language_model import LanguageModel
+# from FARM2.farm.modeling.optimization import initialize_optimizer
+# from FARM2.farm.modeling.prediction_head import TextClassificationHead
+# from FARM2.farm.modeling.tokenization import Tokenizer
+# from FARM2.farm.train import Trainer, EarlyStopping
+# from FARM2.farm.utils import initialize_device_settings, set_all_seeds
+# from farm.data_handler.data_silo import DataSilo
+from farm.data_handler.data_silo import DataSilo
+from farm.data_handler.processor import TextClassificationProcessor
+from farm.infer import Inferencer
+from farm.modeling.adaptive_model import AdaptiveModel
+from farm.modeling.language_model import LanguageModel
+from farm.modeling.optimization import initialize_optimizer
+from farm.modeling.prediction_head import TextClassificationHead
+from farm.modeling.tokenization import Tokenizer
+from farm.train import EarlyStopping, Trainer
+from farm.utils import set_all_seeds, initialize_device_settings
+from algo.models.config.model_args import ClassificationModelArgs
+
+logger = logging.getLogger(__name__)
+
+
+class ClassificationModel:
+    def __init__(self, model_name, args):
+        self.model_name = model_name
+        self.args = self._load_model_args(model_name)
+
+        if isinstance(args, dict):
+            self.args.update_from_dict(args)
+        elif isinstance(args, ClassificationModelArgs):
+            self.args = args
+
+        set_all_seeds(seed=self.args.manual_seed)
+        self.device, self.n_gpu = initialize_device_settings(use_cuda=self.args.use_cuda, use_amp=self.args.use_amp)
+
+    def train_model(self, data_dir):
+        # create tokenizer
+        tokenizer = Tokenizer.load(pretrained_model_name_or_path=self.model_name,
+                                   do_lower_case=self.args.do_lower_case)
+
+        # create a DataProcessor that handles all the conversion from raw text into a pytorch Dataset
+        processor = TextClassificationProcessor(tokenizer=tokenizer,
+                                                max_seq_len=self.args.max_seq_len,
+                                                data_dir=Path(data_dir),
+                                                label_list=self.args.label_list,
+                                                metric=self.args.metric,
+                                                train_filename=self.args.train_filename,
+                                                dev_filename=self.args.dev_filename,
+                                                test_filename=self.args.test_filename,
+                                                dev_split=self.args.dev_split,
+                                                dev_stratification=self.args.dev_stratification,
+                                                delimiter=self.args.delimiter,
+                                                label_column_name=self.args.label_column_name,
+                                                text_column_name=self.args.text_column_name,
+                                                multilabel=self.args.multilabel
+                                                )
+        # create a DataSilo that loads several datasets (train/dev/test), provides DataLoaders for them and calculates a
+        #    few descriptive statistics of our datasets
+        data_silo = DataSilo(
+            processor=processor,
+            max_processes=self.args.max_processes,
+            batch_size=self.args.train_batch_size,
+            eval_batch_size=self.args.eval_batch_size
+        )
+
+        # create an AdaptiveModel
+        language_model = LanguageModel.load(self.model_name)
+        prediction_head = TextClassificationHead(
+            class_weights=data_silo.calculate_class_weights(task_name=self.args.task_name),
+            num_labels=len(self.args.label_list))
+        model = AdaptiveModel(
+            language_model=language_model,
+            prediction_heads=[prediction_head],
+            embeds_dropout_prob=self.args.embeds_dropout_prob,
+            lm_output_types=self.args.lm_output_types,
+            device=self.device)
+
+        # create an optimizer
+        model, optimizer, lr_schedule = initialize_optimizer(
+            model=model,
+            learning_rate=self.args.learning_rate,
+            device=self.device,
+            n_batches=len(data_silo.loaders["train"]),
+            n_epochs=self.args.n_epochs,
+            use_amp=self.args.use_amp)
+
+        early_stopping = None
+        if self.args.use_early_stopping:
+            early_stopping = EarlyStopping(
+                metric=self.args.early_stopping_metric, mode=self.args.early_stopping_mode,
+                save_dir=self.args.model_dir, patience=self.args.early_stopping_patience
+            )
+
+        # feed everything to the Trainer
+        trainer = Trainer(
+            model=model,
+            optimizer=optimizer,
+            data_silo=data_silo,
+            epochs=self.args.n_epochs,
+            n_gpu=self.n_gpu,
+            lr_schedule=lr_schedule,
+            evaluate_every=self.args.evaluate_every,
+            device=self.device,
+            use_amp=self.args.use_amp,
+            grad_acc_steps=self.args.gradient_accumulation_steps,
+            early_stopping=early_stopping,
+            max_grad_norm=self.args.max_grad_norm,
+            train_progress_file=self.args.train_progress_file
+        )
+        trainer.train()
+
+        if not early_stopping:
+            model.save(self.args.model_dir)
+            processor.save(self.args.model_dir)
+
+    def predict(self, texts):
+        model = Inferencer.load(self.args.model_dir)
+        result = model.inference_from_dicts(dicts=texts)
+        model.close_multiprocessing_pool()
+        return result
+
+    def _load_model_args(self, input_dir):
+        args = ClassificationModelArgs()
+        args.load(input_dir)
+        return args
---
+++ algo/models/config/model_args.py
@@ -0,0 +1,81 @@
+# Created by Hansi at 9/23/2021
+import json
+import os
+from dataclasses import dataclass, asdict
+
+
+@dataclass
+class ModelArgs:
+    # device
+    use_cuda: bool = True
+    use_amp: str = None
+    max_processes: int = 1
+    manual_seed: int = 25
+
+    do_lower_case: bool = False
+    max_seq_len: int = 128
+    n_epochs: int = 1
+    train_batch_size: int = 8
+    eval_batch_size: int = 8
+    embeds_dropout_prob: float = 0.1  # The probability that a value in the embeddings returned by the language model will be zeroed.
+    learning_rate: float = 3e-5
+    gradient_accumulation_steps: int = 1
+    max_grad_norm: float = 1.0
+
+    # eval
+    metric = ["f1_macro", "acc"]  # for classification
+    evaluate_every: int = 1
+    use_early_stopping: bool = True
+    early_stopping_metric: str = "loss"
+    early_stopping_mode: str = "min"  # "min" or "max"
+    early_stopping_patience: int = 5
+    model_dir: str = "outputs/model"
+    train_progress_file: str = "outputs/training_progress_scores.csv"
+
+    def update_from_dict(self, new_values):
+        if isinstance(new_values, dict):
+            for key, value in new_values.items():
+                setattr(self, key, value)
+        else:
+            raise (TypeError(f"{new_values} is not a Python dict."))
+
+
+    def get_args_for_saving(self):
+        args_for_saving = {key: value for key, value in asdict(self).items() if key not in self.not_saved_args}
+        return args_for_saving
+
+    def save(self, output_dir):
+        os.makedirs(output_dir, exist_ok=True)
+        with open(os.path.join(output_dir, "model_args.json"), "w") as f:
+            args_dict = self.get_args_for_saving()
+            if args_dict["tokenizer_type"] is not None and not isinstance(args_dict["tokenizer_type"], str):
+                args_dict["tokenizer_type"] = type(args_dict["tokenizer_type"]).__name__
+            json.dump(args_dict, f)
+
+    def load(self, input_dir):
+        if input_dir:
+            model_args_file = os.path.join(input_dir, "model_args.json")
+            if os.path.isfile(model_args_file):
+                with open(model_args_file, "r") as f:
+                    model_args = json.load(f)
+
+                self.update_from_dict(model_args)
+
+
+@dataclass
+class ClassificationModelArgs(ModelArgs):
+    task_name: str = "text_classification"
+    lm_output_types = ["per_sequence"]
+
+    label_list: list = None  # list of labels to predict (strings)
+    train_filename: str = "train.tsv"  # for classification
+    dev_filename: str = None
+    dev_split: float = 0.1
+    dev_stratification = False
+    test_filename: str = None
+    multilabel: bool = False  # set to True for multilabel classification
+    # data format
+    delimiter = "\t"
+    label_column_name = "label"  # for classification
+    text_column_name = "text"  # for classification
+
