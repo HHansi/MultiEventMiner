febe9dbb5ddef4b8276cdc226cb47160f63648c6
HHansi <h.hansi1991@gmail.com>
2021-10-07 10:52:56 +0100

    009: added seed logging

--- experiments/train.py
+++ experiments/train.py
@@ -25,7 +25,7 @@ def train_classifier(data_dir, config):
             os.path.dirname(config['train_progress_file']),
             f"{file_name_splits[0]}_{i}{file_name_splits[1]}")
 
-        set_all_seeds(seed=config['manual_seed'] * (i + 1))
+        set_all_seeds(seed=int(config['manual_seed'] * (i + 1)))
         logger.info(f"Set seed to {int(config['manual_seed'] * (i + 1))}.")
 
         model = ClassificationModel(classifier_config.MODEL_NAME, args=config)
