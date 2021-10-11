e6720b802aae2fa049bca48a9ee3b549c03e91d2
HHansi <h.hansi1991@gmail.com>
2021-10-11 12:20:14 +0100

    009: fixed model path issue

--- experiments/train.py
+++ experiments/train.py
@@ -15,15 +15,17 @@ logger = logging.getLogger(__name__)
 
 def train_classifier(data_dir, config):
     delete_create_folder(classifier_config.OUTPUT_DIRECTORY)
+    base_model_dir = config['model_dir']
+    base_train_progress_file_name = config['train_progress_file']
+    base_file_name_splits = os.path.splitext(base_train_progress_file_name)
 
     for i in config["fold_ids"]:
         # update model dir for the fold
-        config['model_dir'] = f"{config['model_dir']}_{i}"
-        train_progress_file_name = os.path.basename(config['train_progress_file'])
-        file_name_splits = os.path.splitext(train_progress_file_name)
-        config['train_progress_file'] = os.path.join(
-            os.path.dirname(config['train_progress_file']),
-            f"{file_name_splits[0]}_{i}{file_name_splits[1]}")
+        config['model_dir'] = f"{base_model_dir}_{i}"
+        # train_progress_file_name = os.path.basename(config['train_progress_file'])
+        # file_name_splits = os.path.splitext(train_progress_file_name)
+        config['train_progress_file'] = os.path.join(os.path.dirname(config['train_progress_file']),
+                                                     f"{base_file_name_splits[0]}_{i}{base_file_name_splits[1]}")
 
         set_all_seeds(seed=int(config['manual_seed'] * (i + 1)))
         logger.info(f"Set seed to {int(config['manual_seed'] * (i + 1))}.")
@@ -35,14 +37,17 @@ def train_classifier(data_dir, config):
 
 def train_ner(data_dir, config):
     delete_create_folder(ner_config.OUTPUT_DIRECTORY)
+    base_model_dir = config['model_dir']
+    base_train_progress_file_name = config['train_progress_file']
+    base_file_name_splits = os.path.splitext(base_train_progress_file_name)
 
     for i in config["fold_ids"]:
         # update model dir for the fold
-        config['model_dir'] = f"{config['model_dir']}_{i}"
-        train_progress_file_name = os.path.basename(config['train_progress_file'])
-        file_name_splits = os.path.splitext(train_progress_file_name)
+        config['model_dir'] = f"{base_model_dir}_{i}"
+        # train_progress_file_name = os.path.basename(config['train_progress_file'])
+        # file_name_splits = os.path.splitext(train_progress_file_name)
         config['train_progress_file'] = os.path.join(os.path.dirname(config['train_progress_file']),
-                                                     f"{file_name_splits[0]}_{i}{file_name_splits[1]}")
+                                                     f"{base_file_name_splits[0]}_{i}{base_file_name_splits[1]}")
 
         set_all_seeds(seed=int(config['manual_seed'] * (i + 1)))
         logger.info(f"Set seed to {int(config['manual_seed'] * (i + 1))}.")
@@ -59,5 +64,4 @@ if __name__ == '__main__':
 
     # train ner
     # data_dir = os.path.join(ner_config.DATA_DIRECTORY, 'subtask4-token/filtered/farm_format')
-    # train_classifier(data_dir, ner_config.config)
-
+    # train_ner(data_dir, ner_config.config)
