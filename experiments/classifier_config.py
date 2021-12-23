fc1da6b0f7cd209ae2d0133ca26a7ec698e87f77
HHansi <h.hansi1991@gmail.com>
2021-12-23 23:16:26 +0000

    017: Updated configs

--- experiments/classifier_config.py
+++ experiments/classifier_config.py
@@ -15,9 +15,9 @@ SUBMISSION_FILE = os.path.join(PREDICTION_DIRECTORY, 'submission.json')
 
 # Either one (model name or directory) should be provided. Other should be set to None.
 # If both provided, only the model name will be considered.
-MODEL_NAME = None  # "xlm-roberta-large"
-MODEL_DIRECTORY = "/experiments/tranasinghe/MultiEventMiner/trained_models2/sentence/xlm-r-large-en-en"  # Use if multiple models need to be referred during training (model name = model_<fold_id>).
-LANGUAGES = ["en", "es", "pr"]
+MODEL_NAME = "xlm-roberta-large"
+MODEL_DIRECTORY = None  #"/experiments/tranasinghe/MultiEventMiner/trained_models2/sentence/xlm-r-large-en-en"  # Use if multiple models need to be referred during training (model name = model_<fold_id>).
+LANGUAGES = ["en", "pr"]
 
 config = {
     'manual_seed': SEED,
@@ -30,7 +30,7 @@ config = {
     'train_batch_size': 8,
     'eval_batch_size': 8,
     'inference_batch_size': 16,  # 4
-    'evaluate_every': 20,  # 4
+    'evaluate_every': 250,  # 4
     'learning_rate': 1e-5,  # 3e-5
     'metric': ["f1_macro", "acc"],
     'use_early_stopping': True,
