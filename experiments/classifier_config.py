e793d48072d4af885faeadf3067a473908e6ab25
HHansi <h.hansi1991@gmail.com>
2022-01-04 16:26:49 +0000

    017: Updated configs

--- experiments/classifier_config.py
+++ experiments/classifier_config.py
@@ -16,8 +16,8 @@ SUBMISSION_FILE = os.path.join(PREDICTION_DIRECTORY, 'submission.json')
 # Either one (model name or directory) should be provided. Other should be set to None.
 # If both provided, only the model name will be considered.
 MODEL_NAME = None  # "xlm-roberta-large"
-MODEL_DIRECTORY = "/experiments/tranasinghe/MultiEventMiner/trained_models2/token/xlm-r-large-en+es"  # Use if multiple models need to be referred during training (model name = model_<fold_id>).
-LANGUAGES = ["en", "es", "pr"]
+MODEL_DIRECTORY = "/experiments/tranasinghe/MultiEventMiner/trained_models2/token/xlm-r-large-en"  # Use if multiple models need to be referred during training (model name = model_<fold_id>).
+LANGUAGES = ["en", "pr"]
 
 config = {
     'manual_seed': SEED,
