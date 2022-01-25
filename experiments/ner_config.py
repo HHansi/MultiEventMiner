f6b986e21f77ccb655c4418a4168d7b88a06d173
HHansi <h.hansi1991@gmail.com>
2022-01-25 13:12:32 +0000

    017: Updated configs

--- experiments/ner_config.py
+++ experiments/ner_config.py
@@ -16,7 +16,7 @@ SUBMISSION_FILE = os.path.join(PREDICTION_DIRECTORY, 'submission.txt')
 # Either one (model name or directory) should be provided. Other should be set to None.
 # If both provided, only the model name will be considered.
 MODEL_NAME = None  # "xlm-roberta-large"
-MODEL_DIRECTORY = "/experiments/tranasinghe/MultiEventMiner/trained_models2/sentence/xlm-r-large-en+pr+es"  # Use if multiple models need to be referred during training (model name = model_<fold_id>).
+MODEL_DIRECTORY = "/experiments/tranasinghe/MultiEventMiner/trained_models2/sentence/xlm-r-large-en"  # Use if multiple models need to be referred during training (model name = model_<fold_id>).
 LANGUAGES = ["en", "pr", "es"]
 
 config = {
